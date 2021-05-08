9169664e13c279cc0b69402be9f7b4a831b54dc1
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-05-08 17:33:36 -0300

    Adding meta tags to better seo

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,36 @@ import { ToastProvider } from "react-toast-notifications";
 
 function MyApp({ Component, pageProps, company, foods }) {
   return (
+    <>
+       <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charset="utf-8" />
+        <meta name="language" content="pt-BR" />
+        <title>Pix simples</title>
+        <meta name="description" content="Uma forma simples de gerar qrcode pix apenas com a chave e valor. Permitindo fazer isso rápido e fazer o download do qrcode para compartilhar no whatsapp"/>
+        <meta name="robots" content="all"/>
+        <meta name="author" content="Tiago rosa da costa"/>
+        <meta name="keywords" content="Pix, qrcode, simples, envia qrcode pelo whatsapp, qrcode pix para comerciantes, pequenos empreendedores, pagamento rápido e simples "/>
+
+        <meta property="og:type" content="page"/>
+        <meta property="og:url" content=""/>
+        <meta property="og:title" content=""/>
+        <meta property="og:image" content=""/>
+        <meta property="og:description" content=""/>
+
+        <meta property="article:author" content=""/>
+
+        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:site" content="@"/>
+        <meta name="twitter:title" content=""/>
+        <meta name="twitter:creator" content="@"/>
+        <meta name="twitter:description" content=""/>
+
+      </Head>
       <ToastProvider autoDismiss={true} autoDismissTimeout="4000">
         <Component {...pageProps} foods={foods} company={company} />
       </ToastProvider>
+    </>
   )
 }
 
