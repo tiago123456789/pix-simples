aaa55e0a9d3ef9c63fa1b3070682c11163ec1e66
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-05-08 17:14:12 -0300

    Remove elementos desnecessarios

--- components/ExplainProject.js
+++ components/ExplainProject.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default () => (
-<section className="bg-gray-100 dark:bg-gray-900 lg:py-12 lg:flex lg:justify-center">
+                <section className="bg-gray-100 dark:bg-gray-900 lg:py-6 lg:flex lg:justify-center">
                     <div className="bg-white dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg">
-                        <div className="max-w-xl px-6 py-12 text-justify">
+                        <div className="max-w-xl px-6 py-6 text-justify">
                         <h2 className="text-2xl font-bold md:text-3xl font-bold text-xl text-yellow-500">Pix simples</h2>
                         <p className=" text-gray-600 dark:text-gray-400">Pix de forma simples é rápida. Onde é informada a sua chave, nome e valor para gerar o qrcode Pix para pessoa possa ler, outra possibilidade é fazer o download da qrcode para enviar pelo whatsapp.</p>
                         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -9,22 +9,9 @@ import { ToastProvider } from "react-toast-notifications";
 
 function MyApp({ Component, pageProps, company, foods }) {
   return (
-    <div className="items-center">
-      <nav className="bg-white py-2 md:py-4 mb-2" style={{ "border-bottom": "1px solid rgba(0, 0, 0, .1)", }}>
-        <div className="container px-4 mx-auto md:flex md:items-center">
-
-          <div className="flex justify-between items-center">
-            <a href="#" className="font-bold text-xl text-center text-yellow-500">Pix Simples</a>
-          </div>
-
-          <div className="md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" >
-          </div>
-        </div>
-      </nav>
       <ToastProvider autoDismiss={true} autoDismissTimeout="4000">
         <Component {...pageProps} foods={foods} company={company} />
       </ToastProvider>
-    </div>
   )
 }
 
